e4839f1c15c40fa709c5b197b4aca2a208e2bb12
Myles Javillonar <152746324+mylesjav@users.noreply.github.com>
2024-06-27 23:26:09 -0700

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
-
-router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
-
+const router = require("express").Router();
+const user = require("./user-routes");
+const post = require("./post-routes");
+const comment = require("./comment-routes");
+router.use("/users", user);
+router.use("/posts", post);
+router.use("/comments", comment);
 module.exports = router;
